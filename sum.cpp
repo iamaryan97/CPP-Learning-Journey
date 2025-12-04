16272f8b1d11423e3bae103c23eed16227b15211
Aryan Nalawade <balaji@Aryans-MacBook-Pro.local>
2025-12-04 19:14:14 +0530

    Day- 1 programs

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter two numbers:"<<endl;
+    cin>>a;
+    cin>>b;
+    sum=a+b;
+    cout<<"sum="<<sum<<endl;
+    return 0;
+    
+
+}
